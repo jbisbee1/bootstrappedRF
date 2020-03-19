8793eac29befa5c922ceb2b496714f230e7690f1
zjl0714 <zjl0714@gmail.com>
2020-03-19 19:59:22 -0400

    update code

--- bootstrap_RF_demonstration.R
+++ bootstrap_RF_demonstration.R
@@ -37,17 +37,18 @@ for(i in 1:50) {
     rang.bsT <- ranger(Species ~., data = iris[inds,],probability = T)
     bart.bsT <- bartMachine(X = iris[inds,-5],y = as.integer(as.factor(iris[inds,5])),verbose = F)
     grf.bsT <- regression_forest(X = iris[inds,-5],Y = as.integer(as.factor(iris[inds,5])))
-    grf.bsT.err <- mean((as.integer(as.factor(iris[inds,5])) - predict(grf.bsT)$predictions)^2)
+    grf.bsT.err <- sqrt(mean((as.integer(as.factor(iris[inds,5])) - predict(grf.bsT)$predictions)^2))
     
   # replace = F
     inds <- sample(1:nrow(iris),round(nrow(iris)*.6),replace = F)
     rang.bsF <- ranger(Species ~., data = iris[inds,],probability = T)
     bart.bsF <- bartMachine(X = iris[inds,-5],y = as.integer(as.factor(iris[inds,5])),verbose = F)
     grf.bsF <- regression_forest(X = iris[inds,-5],Y = as.integer(as.factor(iris[inds,5])))
-    grf.bsF.err <- mean((as.integer(as.factor(iris[inds,5])) - predict(grf.bsF)$predictions)^2)
+    grf.bsF.err <- sqrt(mean((as.integer(as.factor(iris[inds,5])) - predict(grf.bsF)$predictions)^2))
     
     perf.bs <- bind_rows(perf.bs,
-                         data.frame(err = c(rang.bsT$prediction.error,rang.bsF$prediction.error),
+                         data.frame(err = c(sqrt(rang.bsT$prediction.error),
+                                            sqrt(rang.bsF$prediction.error)),
                                     bsType = c("replace=T","replace=F"),
                                     meth = "ranger"),
                          data.frame(err = c(bart.bsT$rmse_train,bart.bsF$rmse_train),
@@ -69,7 +70,7 @@ perf.bs %>%
 # Simulated data
 n = 2000; p = 10
 X = matrix(rnorm(n*p), n, p)
-W = rbinom(n, 1, 0.5)
+W = rbinom(n, 1, 0.5) 
 Y = pmax(X[,1], 0) * W + X[,2] + pmin(X[,3], 0) + rnorm(n)
 
 
@@ -80,17 +81,18 @@ for(i in 1:50) {
   rang.bsT <- ranger(Y ~., data = data.frame(X[inds,],Y = Y[inds]))
   bart.bsT <- bartMachine(X = data.frame(X[inds,]),y = Y[inds],verbose = F)
   grf.bsT <- regression_forest(X = X[inds,],Y = Y[inds])
-  grf.bsT.err <- mean((Y[inds] - predict(grf.bsT)$predictions)^2)
+  grf.bsT.err <- sqrt(mean((Y[inds] - predict(grf.bsT)$predictions)^2))
   
   # replace = F
   inds <- sample(1:nrow(X),round(nrow(X)*.6),replace = F)
   rang.bsF <- ranger(Y ~., data = data.frame(X[inds,],Y = Y[inds]))
   bart.bsF <- bartMachine(X = data.frame(X[inds,]),y = Y[inds],verbose = F)
   grf.bsF <- regression_forest(X = X[inds,],Y = Y[inds])
-  grf.bsF.err <- mean((Y[inds] - predict(grf.bsF)$predictions)^2)
+  grf.bsF.err <- sqrt(mean((Y[inds] - predict(grf.bsF)$predictions)^2))
   
   perf.bs <- bind_rows(perf.bs,
-                       data.frame(err = c(rang.bsT$prediction.error,rang.bsF$prediction.error),
+                       data.frame(err = c(sqrt(rang.bsT$prediction.error),
+                                          sqrt(rang.bsF$prediction.error)),
                                   bsType = c("replace=T","replace=F"),
                                   meth = "ranger"),
                        data.frame(err = c(bart.bsT$rmse_train,bart.bsF$rmse_train),
